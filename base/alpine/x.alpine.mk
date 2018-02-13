a67a8cbd1109672f4f1539562a9a09ebe5703dd7
Patrick Stewart <mrpastewart@yahoo.com>
2018-02-12 21:51:28 -0700

    Update: fixing some minor issues in base makefile

--- base/alpine/x.alpine.mk
+++ base/alpine/x.alpine.mk
@@ -12,6 +12,13 @@ BASE_IMAGE_OS_NAME	?= Alpine Linux
 BASE_IMAGE_OS_URL	?= https://alpinelinux.org
 
 
+### BUILD #######################################################################
+
+
+BUILD_VARS		+= SU_EXEC_VERSION \
+
+			   TINI_VERSION
+
 ### MK_DOCKER_IMAGE #############################################################
 
 include $(PROJECT_DIR)/x.version.mk
--- base/arch/x.arch.mk
+++ base/arch/x.arch.mk
@@ -14,6 +14,13 @@ BASE_IMAGE_OS_URL	?= https://archlinux.org
 
 
 
+### BUILD #######################################################################
+
+
+BUILD_VARS		+= SU_EXEC_VERSION \
+
+			   TINI_VERSION
+
 ### MK_DOCKER_IMAGE #############################################################
 
 include $(PROJECT_DIR)/x.version.mk
