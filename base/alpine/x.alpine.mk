de4f0e82a4326387885fe6d15328ba73666aa58c
Patrick Stewart <mrpastewart@yahoo.com>
2018-02-12 20:50:33 -0700

    Update: working on alpine build system

--- base/alpine/x.alpine.mk
+++ base/alpine/x.alpine.mk
@@ -1,7 +1,6 @@
 ### BASE_IMAGE #################################################################
 
 
-
 BASE_IMAGE_NAME		?= alpine
 
 BASE_IMAGE_TAG		?= $(BASE_IMAGE_OS_VERSION)
@@ -13,10 +12,8 @@ BASE_IMAGE_OS_NAME	?= Alpine Linux
 BASE_IMAGE_OS_URL	?= https://alpinelinux.org
 
 
-### BUILD #######################################################################
-
-
 ### MK_DOCKER_IMAGE #############################################################
 
+include $(PROJECT_DIR)/x.version.mk
 
 #################################################################################
