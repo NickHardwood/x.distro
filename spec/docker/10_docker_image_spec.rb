714805ff92f2a8d5c62c2ce286ac59dd91bcc089
Patrick Stewart <mrpastewart@yahoo.com>
2018-02-18 16:28:22 -0700

    Added: add spec for docker images

---
+++ spec/docker/10_docker_image_spec.rb
@@ -0,0 +1,200 @@
+require "docker_helper"
+
+### DOCKER_IMAGE ###############################################################
+
+describe "Docker image", :test => :docker_image do
+  # Default Serverspec backend
+  before(:each) { set :backend, :docker }
+
+  ### DOCKER_IMAGE #############################################################
+
+  describe docker_image(ENV["DOCKER_IMAGE"]) do
+    # Execute Serverspec commands locally
+    before(:each) { set :backend, :exec }
+    it { is_expected.to exist }
+  end
+
+  ### OS #######################################################################
+
+  describe "Operating system" do
+    context "family" do
+      # We can not simple test the os[:family] because CentOS is reported as "redhat"
+      subject { file("/etc/#{ENV["BASE_IMAGE_NAME"]}-release") }
+      it "sould eq \"#{ENV["BASE_IMAGE_NAME"]}\"" do
+        expect(subject).to be_file
+      end
+    end
+    context "release" do
+      subject { os[:release] }
+      it { is_expected.to match(/^#{Regexp.escape(ENV["BASE_IMAGE_OS_VERSION"])}\./) }
+    end
+    context "locale" do
+      context "CHARSET" do
+        subject { command("echo ${CHARSET}") }
+        it { expect(subject.stdout.strip).to eq("UTF-8") }
+      end
+      context "LANG" do
+        subject { command("echo ${LANG}") }
+        it { expect(subject.stdout.strip).to eq("en_US.UTF-8") }
+      end
+      context "LC_ALL" do
+        subject { command("echo ${LC_ALL}") }
+        it { expect(subject.stdout.strip).to eq("en_US.UTF-8") }
+      end
+    end
+  end
+
+  ### PACKAGES #################################################################
+
+  describe "Packages" do
+
+    # [package, version, installer]
+    packages = []
+
+    case ENV["BASE_IMAGE_NAME"]
+    when "alpine"
+      packages += [
+        "bash",
+        "ca-certificates",
+        "curl",
+        "jq",
+        "libressl",
+        "nmap-ncat",
+        "su-exec",
+        "supervisor",
+        "tini",
+      ]
+    when "centos"
+      packages += [
+        "bash",
+        "bind-utils",
+        "ca-certificates",
+        "curl",
+        "iproute",
+        "jq",
+        "less",
+        "net-tools",
+        # CentOS 7 contains ncat an obsoleted version of ncat,
+        # docker-entrypoint.d/02-wait-for.sh requires the ncat version 7.x
+        # "nmap-ncat",
+        "ncat",
+        "openssl",
+        "supervisor",
+        "which",
+      ]
+    when "debian"
+      packages += [
+        "bash",
+        "ca-certificates",
+        "curl",
+        "jq",
+        "less",
+        "net-tools",
+        "nmap",
+        "openssl",
+        "procps",
+        "supervisor",
+      ]
+    end
+
+    packages.each do |package, version, installer|
+      describe package(package) do
+        it { is_expected.to be_installed }                        if installer.nil? && version.nil?
+        it { is_expected.to be_installed.with_version(version) }  if installer.nil? && ! version.nil?
+        it { is_expected.to be_installed.by(installer) }          if ! installer.nil? && version.nil?
+        it { is_expected.to be_installed.by(installer).with_version(version) } if ! installer.nil? && ! version.nil?
+      end
+    end
+  end
+
+  ### COMMANDS #################################################################
+
+  describe "Commands" do
+
+    # [command, version, args]
+    commands = []
+
+    case ENV["BASE_IMAGE_NAME"]
+    when "centos"
+      commands += [
+        "/sbin/su-exec",
+        ["/sbin/tini",              ENV["TINI_VERSION"]],
+      ]
+    when "debian"
+      commands += [
+        "/sbin/su-exec",
+        ["/sbin/tini",              ENV["TINI_VERSION"]],
+      ]
+    end
+
+    commands.each do |command, version, args|
+      describe "Command \"#{command}\"" do
+        subject { file(command) }
+        let(:version_regex) { "(^|\\W)#{version}(\\W|$)" }
+        let(:version_cmd) { "#{command} #{args.nil? ? "--version" : "#{args}"}" }
+        it "should be installed#{version.nil? ? "" : " with version \"#{version}\""}" do
+          expect(subject).to exist
+          expect(subject).to be_executable
+          expect(command(version_cmd).stdout).to match(version_regex) unless version.nil?
+        end
+      end
+    end
+  end
+
+  ### FILES ####################################################################
+
+  describe "Files" do
+
+    # [file, mode, user, group, [expectations]]
+    files = [
+      ["/docker-entrypoint.sh",                           755, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d",                            755, "root", "root", [:be_directory]],
+      ["/docker-entrypoint.d/01-lib-messages.sh",         644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/02-lib-wait-for.sh",         644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/10-default-command.sh",      644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/20-docker-introspection.sh", 644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/30-simple-ca-wait.sh",       644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/35-certs-environment.sh",    644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/40-server-key-pwd.sh",       644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/41-simple-ca-certs.sh",      644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/45-trusted-ca-certs.sh",     644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/46-server-pkcs12.sh",        644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/docker-entrypoint.d/90-exec-command.sh",         644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/etc/bashrc",                                     644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/etc/inputrc",                                    644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/etc/profile",                                    644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/etc/ssl/openssl.cnf",                            644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/etc/supervisor",                                 755, "root", "root", [:be_directory]],
+      ["/etc/supervisor/supervisord.ini",                 644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/etc/supervisor/listener",                        755, "root", "root", [:be_directory]],
+      ["/etc/supervisor/listener/terminate-container",    755, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/root/.bash_logout",                              644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/root/.bash_profile",                             644, "root", "root", [:be_file, :eq_sha256sum]],
+      ["/var/log/docker.log",                             nil, "root", "root", [:be_pipe]],
+      ["/var/log/docker.err",                             nil, "root", "root", [:be_pipe]],
+    ]
+
+    files.each do |file, mode, user, group, expectations|
+      expectations ||= []
+      context file(file) do
+        it { is_expected.to exist }
+        it { is_expected.to be_file }       if expectations.include?(:be_file)
+        it { is_expected.to be_pipe }       if expectations.include?(:be_pipe)
+        it { is_expected.to be_directory }  if expectations.include?(:be_directory)
+        it { is_expected.to be_mode(mode) } unless mode.nil?
+        it { is_expected.to be_owned_by(user) } unless user.nil?
+        it { is_expected.to be_grouped_into(group) } unless group.nil?
+        its(:sha256sum) do
+          is_expected.to eq(
+              Digest::SHA256.file("rootfs/#{subject.name}").to_s
+          )
+        end if expectations.include?(:eq_sha256sum)
+      end
+    end
+  end
+
+  ##############################################################################
+
+end
+
+################################################################################
---
+++ spec/docker/20_docker_container_spec.rb
@@ -0,0 +1,149 @@
+require "docker_helper"
+
+### DOCKER_CONTAINER ###########################################################
+
+describe "Docker container", :test => :docker_container do
+  # Default Serverspec backend
+  before(:each) { set :backend, :docker }
+
+  ### DOCKER_CONTAINER #########################################################
+
+  describe docker_container(ENV["CONTAINER_NAME"]) do
+    # Execute Serverspec command locally
+    before(:each) { set :backend, :exec }
+    it { is_expected.to be_running }
+  end
+
+  ### PROCESSES ################################################################
+
+  describe "Processes" do
+    # [process, user, group, pid]
+    processes = []
+
+    case ENV["BASE_IMAGE_NAME"]
+    when "alpine"
+      processes += [
+        ["/sbin/tini",              "root", "root", 1],
+      ]
+    when "centos"
+      processes += [
+        ["tini",                    "root", "root", 1],
+      ]
+    end
+
+    processes.each do |process, user, group, pid|
+      context process(process) do
+        it { is_expected.to be_running }
+        its(:pid) { is_expected.to eq(pid) } unless pid.nil?
+        its(:user) { is_expected.to eq(user) } unless user.nil?
+        its(:group) { is_expected.to eq(group) } unless group.nil?
+      end
+    end
+  end
+
+  ### DOCKER_ENTRYPOINT ########################################################
+
+  describe "Docker Entrypoint", :test => :entrypoint do
+    lib_messages = "/docker-entrypoint.d/01-lib-messages.sh"
+    lib_wait_for = "/docker-entrypoint.d/02-lib-wait-for.sh"
+
+    ### /docker-entrypoint.d/01-lib-messages.sh ################################
+
+    describe lib_messages do
+      describe "#msg" do
+        context "works with default arguments" do
+          subject { command(<<~END)
+            /bin/bash -c ". #{lib_messages}; msg LEVEL bash message"
+            END
+          }
+          its(:exit_status) { is_expected.to eq(0) }
+          its(:stdout) { is_expected.to match(/^\[\d+-\d+-\d+T\d+:\d+:\d+Z\s*\]\[LEVEL\s*\]\[bash\s*\] message$/) }
+        end
+      end
+      [
+        "error",
+        "warn",
+        "info",
+        "debug",
+      ].each do |function|
+        describe "##{function}" do
+          context "works with default arguments" do
+            subject { command(<<~END)
+              /bin/bash -c ". #{lib_messages}; #{function} message"
+              END
+            }
+            its(:exit_status) { is_expected.to eq(0) }
+            its(:stdout) { is_expected.to match(/^\[\d+-\d+-\d+T\d+:\d+:\d+Z\s*\]\[#{function.upcase}\s*\]\[bash\s*\] message$/) }
+          end
+        end
+      end
+    end
+
+    ### /docker-entrypoint.d/02-lib-wait-for.sh ################################
+
+    describe lib_wait_for do
+      context "#wait_for_dns" do
+        # [url,                                   exit_status, match]
+        [
+          ["simple-ca.local",                     0, "Got the simple-ca.local address \\d+\\.\\d+\\.\\d+\\.\\d+ in 0s"],
+          ["https://simple-ca.local:443/ca.crt",  0, "Got the simple-ca.local address \\d+\\.\\d+\\.\\d+\\.\\d+ in 0s"],
+          ["https://nonexistent.local:8888/test", 1, "nonexistent.local name resolution timed out after \\d+s"],
+        ].each do |url, exit_status, match|
+          context "resolve \"#{url}\"" do
+            subject { command(<<~END)
+              /bin/bash -c ". #{lib_messages}; . #{lib_wait_for}; wait_for_dns 1 #{url}"
+              END
+            }
+            its(:exit_status) { is_expected.to eq(exit_status) }
+            its(:stdout) { is_expected.to match(/#{match}/) }
+          end
+        end
+      end
+
+      context "#wait_for_tcp" do
+        # [url,                                   exit_status, match]
+        [
+          ["baseimage.local:7",                   0, "Got the connection to tcp://baseimage.local:7 in 0s"],
+          ["simple-ca.local:443",                 0, "Got the connection to tcp://simple-ca.local:443 in 0s"],
+          ["https://simple-ca.local:443/ca.crt",  0, "Got the connection to tcp://simple-ca.local:443 in 0s"],
+          ["https://simple-ca.local/ca.crt",      0, "Got the connection to tcp://simple-ca.local:443 in 0s"],
+          ["http://simple-ca.local/ca.crt",       1, "Connection to tcp://simple-ca.local:80 timed out after \\d+s"],
+          ["https://nonexistent.local:8888/test", 1, "nonexistent.local name resolution timed out after \\d+s"],
+        ].each do |url, exit_status, match|
+          context "connect to \"#{url}\"" do
+            subject { command(<<~END)
+              /bin/bash -c ". #{lib_messages}; . #{lib_wait_for}; wait_for_tcp 1 #{url}"
+              END
+            }
+            its(:exit_status) { is_expected.to eq(exit_status) }
+            its(:stdout) { is_expected.to match(/#{match}/) }
+          end
+        end
+      end
+
+      context "#wait_for_url" do
+        # [url,                                   exit_status, match]
+        [
+          ["https://simple-ca.local/ca.crt",      0, "Got the connection to https://simple-ca.local/ca.crt in 0s"],
+          ["http://simple-ca.local/ca.crt",       1, "Connection to http://simple-ca.local/ca.crt timed out after \\d+s"],
+          ["https://nonexistent.local:8888/test", 1, "nonexistent.local name resolution timed out after \\d+s"],
+        ].each do |url, exit_status, match|
+          context "connect to \"#{url}\"" do
+            subject { command(<<~END)
+              /bin/bash -c ". #{lib_messages}; . #{lib_wait_for}; wait_for_url 1 #{url}"
+              END
+            }
+            its(:exit_status) { is_expected.to eq(exit_status) }
+            its(:stdout) { is_expected.to match(/#{match}/) }
+          end
+        end
+      end
+    end
+
+  end
+
+  ##############################################################################
+
+end
+
+################################################################################
